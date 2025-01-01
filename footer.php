52e161fccb289220ed36eb9492f2ae83910ea32d
unknown <ju.velling@gmail.com>
2025-01-01 09:37:02 +0100

    added footer

--- footer.php
+++ footer.php
@@ -1,40 +1,18 @@
     </main>
 
-    <?php
-
-        if(function_exists('the_custom_logo')) {
-
-            //the_custom_logo();
-            $custom_logo_id = get_theme_mod('custom_logo');
-            $logo = wp_get_attachment_image_src($custom_logo_id);
-
-        }
-
-    ?>
-
     <footer>
-        <div class="logo">
-            <img src="<?php echo $logo[0] ?>" alt="Logo" />
-        </div>
-        <div class="footer-wrapper">
 
+        <?php
+
+            dynamic_sidebar( 'footer-widget' ); 
+        
+        ?>
+
+        <div class="theme-info">
             <?php
-
-                wp_nav_menu(
-                    array(
-                        'menu' => 'footer',
-                        'container' => '',
-                        'theme_location' => 'footer',
-                        'items_wrap' => '<ul class="footer-navigation">%3$s</ul>',
-                        'fallback_cb' => '',
-                        'depth' => 2
-                    )
-                );
-
-                dynamic_sidebar( 'footer-widget' ); 
-            
+                $theme = wp_get_theme();
             ?>
-
+            Wordpress Theme: <?php echo $theme->get( 'Name' ); ?> by <?php echo $theme->get( 'Author' ); ?>
         </div>
     </footer>
 
